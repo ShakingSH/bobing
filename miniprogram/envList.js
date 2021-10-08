4c705d143a5397c5d0f08c9da238621753f71730
ShakingSH <1731013090@qq.com>
2021-10-08 23:53:57 +0800

    页面间数据交互、js文件注释

--- miniprogram/envList.js
+++ miniprogram/envList.js
@@ -1,4 +1,4 @@
-const envList = [{"envId":"cloud1-5gheuamm43400229","alias":"cloud1"}]
+const envList = [{ "envId": "cloud1-5gheuamm43400229", "alias": "cloud1" }]
 const isMac = false
 module.exports = {
     envList,
